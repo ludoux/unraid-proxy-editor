d22140651ab247bfe1914ec8d36881765f6d0128
Lu Chang <chinaluchang@live.com>
2022-12-22 00:46:38 +0800

    pkg: 2022.12.22a
    add: profile and wget proxy env
    fix: wipe cfg file after reboot

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ https://forums.unraid.net/topic/52623-plugin-system-documentation/
 https://github.com/llalon/unraid-plugin-composerize
 
 https://github.com/dorgan/Unraid-networkstats
+
+https://www.assatur.com/archives/%E8%A7%A3%E5%86%B3unraid%E8%AE%BF%E9%97%AE%E5%BA%94%E7%94%A8%E5%95%86%E5%BA%97%E5%8F%8A%E7%B3%BB%E7%BB%9F%E6%9B%B4%E6%96%B0%E6%97%B6%E7%BD%91%E7%BB%9C%E8%BF%9E%E6%8E%A5%E5%A4%B1%E8%B4%A5%E7%9A%84%E9%97%AE%E9%A2%98
--- src/usr/local/emhttp/plugins/proxy.editor/include/try.php
+++ src/usr/local/emhttp/plugins/proxy.editor/include/try.php
@@ -21,13 +21,34 @@ if ($_POST['ca_active_config'] == "-1") {
     exec("echo -e \"tunnel=1\nport=$port\nproxy=$addr\" > /boot/config/plugins/community.applications/proxy.cfg");
 }
 
-$defaulttext = "\/usr\/local\/sbin\/emhttp &";
-if ($_POST['emhttp_active_config'] == "-1") {
-    exec("sed -i \"/$defaulttext/c$defaulttext\" /boot/config/go");
+# delete lines contained # Added by ProxyEditor
+# sed -i "/# Added by ProxyEditor/d" /boot/config/go
+exec("sed -i \"/# Added by ProxyEditor/d\" /boot/config/go");
+
+$emhttpdefault = "\/usr\/local\/sbin\/emhttp &";
+if ($_POST['sys_active_config'] == "-1") {
+    exec("sed -i \"/$emhttpdefault/c$emhttpdefault\" /boot/config/go");
  } else {
-     $emhttpid = $_POST['emhttp_active_config'];
-     $url = $cfg["emhttp_config_"."$emhttpid"."_url"];
-     $replacement = "http_proxy="."$url"." https_proxy="."$url "."$defaulttext";
-     exec("sed -i \"/$defaulttext/c$replacement\" /boot/config/go");
- }
+    # /boot/config/go: emhttp
+    $sysid = $_POST['sys_active_config'];
+    $url = $cfg["sys_config_"."$sysid"."_url"];
+    $emhttpreplace = "http_proxy="."$url"." https_proxy="."$url "."$emhttpdefault";
+    exec("sed -i \"/$emhttpdefault/c$emhttpreplace\" /boot/config/go");
+    
+    # /boot/config/go: set http(s)_proxy in /etc/profile
+    $intext = "echo " . "\\\"" . "export http_proxy=" . "\\\\" . "\\\"" . "$url" . "\\\\" . "\\\" >> /etc/profile";
+    exec("echo \"" . "$intext" . " # Added by ProxyEditor\" >> /boot/config/go");
+    $intext = "echo " . "\\\"" . "export https_proxy=" . "\\\\" . "\\\"" . "$url" . "\\\\" . "\\\" >> /etc/profile";
+    exec("echo \"" . "$intext" . " # Added by ProxyEditor\" >> /boot/config/go");
+
+    # /boot/config/go: set wget wait time and proxy settings in /root/.wgetrc
+    $intext = "echo " . "\\\"" . "wait=10" . "\\\" >> /root/.wgetrc";
+    exec("echo \"" . "$intext" . " # Added by ProxyEditor\" >> /boot/config/go");
+    $intext = "echo " . "\\\"" . "use_proxy=yes" . "\\\" >> /root/.wgetrc";
+    exec("echo \"" . "$intext" . " # Added by ProxyEditor\" >> /boot/config/go");
+    $intext = "echo " . "\\\"" . "export http_proxy=" . "\\\"" . "$url" . "\\\" >> /root/.wgetrc";
+    exec("echo \"" . "$intext" . " # Added by ProxyEditor\" >> /boot/config/go");
+    $intext = "echo " . "\\\"" . "export https_proxy=" . "\\\"" . "$url" . "\\\" >> /root/.wgetrc";
+    exec("echo \"" . "$intext" . " # Added by ProxyEditor\" >> /boot/config/go");
+}
 ?>
