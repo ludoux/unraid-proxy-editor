010b77a9ce7dd5b67a97463c52aac4f401905a37
Lu Chang <chinaluchang@live.com>
2022-12-22 11:21:34 +0800

    improve description

--- src/usr/local/emhttp/plugins/proxy.editor/scripts/view_proxy_files.sh
+++ src/usr/local/emhttp/plugins/proxy.editor/scripts/view_proxy_files.sh
@@ -8,6 +8,21 @@ else
     echo "CA proxy.cfg 文件(/boot/config/plugins/community.applications/proxy.cfg)不存在, CA当前没有使用代理"
 fi
 
+echo ""
 echo "======================cat /boot/config/go======================"
 echo -e "`sed 's/\r$//' /boot/config/go`"
 echo "==============================END=============================="
+
+echo ""
+echo "=======================cat /etc/profile========================"
+echo -e "`sed 's/\r$//' /etc/profile`"
+echo "==============================END=============================="
+
+echo ""
+if [ -f "/root/.wgetrc" ]; then
+    echo "=======================cat /root/.wgetrc======================="
+    echo -e "`sed 's/\r$//' /root/.wgetrc`"
+    echo "==============================END=============================="
+else 
+    echo "wget 配置文件(/root/.wgetrc)不存在, wget 当前没有使用代理"
+fi
